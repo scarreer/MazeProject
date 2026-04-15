61d4ea717f462a2d14412d1773b5c237e0011835
footballdavis422-coder <footballdavis422@gmail.com>
2026-04-15 13:29:48 -0600

    Update MazeCell.java

--- src/classes/MazeCell.java
+++ src/classes/MazeCell.java
@@ -1,5 +1,20 @@
 package classes;
 
 public class MazeCell {
-
+	private int x;
+	private int y;
+	private boolean filled;
+	
+	public boolean isFilled() {
+		return filled;
+	}
+	
+	public void setFilled(boolean fill) {
+		filled = fill;
+		if(fill) {
+			//Color black
+		}else {
+			//Color white
+		}
+	}
 }
