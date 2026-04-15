0a2d233dec7b6593f2b6affe8aeec3161a918a10
scarreer <88905746+scarreer@users.noreply.github.com>
2026-04-15 12:15:07 -0600

    Initial commit

---
+++ src/classes/Raycasting.java
@@ -0,0 +1,14 @@
+package classes;
+
+import edu.princeton.cs.algs4.StdDraw;
+
+public class Raycasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		StdDraw.line(10, 8090, 8397, 80); 
+
+	}
+
+}
