bc1a146e0b89c32dc3b6f14e30da6bbc6961fd68
footballdavis422-coder <footballdavis422@gmail.com>
2026-04-20 10:02:44 -0600

    Update MazeCell.java
    Added some fields for additional functions.

--- src/classes/MazeCell.java
+++ src/classes/MazeCell.java
@@ -1,31 +1,58 @@
-package classes;
+package classes
 
-/**
-* An individual maze cell in the maze
-*
-* @author Davis Martin
-*/
-public class MazeCell {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class MazeCell extends JButton {
+	private static final long serialVersionUID = 8758030830332429424L;
+	
 	private int x;
 	private int y;
 	private boolean filled;
-
-	/**
-	* Returns whether the cell is filled.
-	*/
+	private boolean isBorder;
+	
+	public MazeCell(int x, int y, boolean filled, Dimension size, char type) {
+		this.x = x;
+		this.y = y;
+		this.setFilled(filled);
+		if(filled) {
+			this.isBorder = true;
+		}else {
+			this.isBorder = false;
+		}
+		this.setPreferredSize(size);
+		
+		switch(type) {
+		case 's': this.setBackground(Color.GREEN); this.isBorder = true; break;
+		case 'e': this.setBackground(Color.RED); this.isBorder = true; break;
+		}
+		
+		this.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(!isBorder())
+					setFilled(!isFilled());
+			}
+		});
+	}
+	
+	public boolean isBorder() {
+		return isBorder;
+	}
+	
 	public boolean isFilled() {
 		return filled;
 	}
-
-	/**
-	* Sets the cell to be filled or not.
-	*/
+	
 	public void setFilled(boolean fill) {
 		filled = fill;
 		if(fill) {
-			//Color black
+			this.setBackground(Color.BLACK);
 		}else {
-			//Color white
+			this.setBackground(Color.WHITE);
 		}
 	}
 }
