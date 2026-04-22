22f9f1ecc2b1487dce37277523748b4fd3c15492
footballdavis422-coder <footballdavis422@gmail.com>
2026-04-22 10:06:26 -0600

    Update Maze.java

--- src/classes/Maze.java
+++ src/classes/Maze.java
@@ -2,14 +2,23 @@ package classes;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import edu.princeton.cs.algs4.DepthFirstPaths;
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class Maze {
 
 	public static void main(String[] args) {
-		
-
+		String s = "010";
+		int i = Integer.parseInt(s);
+		System.out.println(s);
+		System.out.println(i);
 	}
 	
 	public static int[][] generateMaze() {
@@ -54,18 +63,63 @@ public class Maze {
 	public static void solveMaze() {
 		int[][] array = mazeToArray(Gui.getMaze());
 		
+		//Writes graph.txt
 		try (FileWriter writer = new FileWriter("src/classes/resources/graph.txt")) {
+			//Helpers
+			StringBuilder sb = new StringBuilder();
+			Map<String, Integer> indexMap = new HashMap<>();
+			List<int[]> reverseMap = new ArrayList<>();
+			int vertices = 0;
+			int edges = 0;
+			
+			//Maps out the vertices to prepare for graphing
 			for(int i = 0; i < 11; i++) {
 				for(int j = 0; j < 11; j++) {
-					writer.write("" + i + "," + j + "\n");
+					if(array[i][j] == 0) { // array[i][j] != 1 to include start and end points
+						String key = i + "," + j;
+						indexMap.put(key, vertices);
+						reverseMap.add(new int[] {i, j});
+						vertices++;
+					}
 				}
 			}
 			
-            writer.write("Hello, world!");
+			//Prepares to write vertices and edges into file
+			for(int i = 0; i < 11; i++) {
+				for(int j = 0; j < 11; j++) {
+					if(array[i][j] == 0) {
+						String key = i + "," + j;
+						int vertex = indexMap.get(key);
+						
+						//Is there a path down?
+						if(array[i+1][j] == 0) {
+							edges++;
+							int neighbor = indexMap.get((i+1) + "," + j);
+							sb.append(vertex + " " + neighbor + "\n");
+						}
+						
+						//Is there a path right?
+						if(array[i][j+1] == 0) {
+							edges++;
+							int neighbor = indexMap.get(i + "," + (j+1));
+							sb.append(vertex + " " + neighbor + "\n");
+						}
+					}
+				}
+			}
+			
+			
+			writer.write(vertices + "\n");
+			writer.write(edges + "\n");
+			writer.write(sb.toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+		
+		//Creates graph
+		Graph graph = new Graph(new In("src/classes/resources/graph.txt"));
+		DepthFirstPaths dfs = new DepthFirstPaths(graph, 0);
+		System.out.println(dfs.pathTo(48));
 	}
 
 	private static int[][] mazeToArray(MazeCell[][] maze){
